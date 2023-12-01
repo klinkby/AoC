620139136266d386ab81cba208d8273fce430ff5
Mads Klinkby <m@kli.dk>
2023-12-01 08:53:16 +0100

    Puzzle 1

---
+++ src/2023/01/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+/// Read the input text file,
+/// concat the first and last digit on each line
+/// parse number 
+/// and aggegate its sum.
+
+var sum = File.ReadAllLines(@"input.txt")
+              .Select(line => string.Empty + Regex.Match(line, @"\d").Value + Regex.Match(line, @"\d", RegexOptions.RightToLeft).Value)
+              .Select(int.Parse)
+              .Aggregate((a,b) => a + b);
+
+Console.WriteLine(sum);
