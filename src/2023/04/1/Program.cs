a8d754e9abf535406527efd32599030a1ac136da
Mads Klinkby <m@kli.dk>
2023-12-04 08:56:33 +0100

    Day 4 puzzle 1

---
+++ src/2023/04/1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Read the input text file,
+// Parse the lines into winning numbers and your numbers,
+// Intersect the two sets of numbers,
+// calculate points as 2^count,
+// then summarize.
+
+var regex = new Regex(@"(?::)(?:\s+(?'win'\d+))+(?:\s\|)(?:\s+(?'you'\d+))+$",
+    RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+
+var points =
+    from line in File.ReadAllLines(@"..\..\..\..\input.txt")
+    from m in regex.Matches(line)
+    let win = m.Groups["win"].Captures.Select(c => int.Parse(c.Value))
+    let you = m.Groups["you"].Captures.Select(c => int.Parse(c.Value))
+    let count = win.Intersect(you).Count()
+    select count == 0 ? 0 : 1 << (count - 1);
+
+var sum = points.Sum();
+Debug.Assert(17782 == sum);
+Console.WriteLine(sum);
---
+++ src/2023/04/README.md
@@ -0,0 +1,4 @@
+Day 4: Scratchcards
+---
+
+https://adventofcode.com/2023/day/4
