da193e2c40f699817f6193f39af8eb3ecee67386
Mads Klinkby <m@kli.dk>
2023-12-04 13:57:08 +0100

    Day 4 puzzle 2

---
+++ src/2023/04/2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Read the input text file,
+// Parse the lines into winning numbers and your numbers,
+// Intersect the two sets of numbers to find the wins on each card.
+// Iterate the cards, increment the following (wins) cards, 
+// then summarize.
+
+var regex = new Regex(@"(?::)(?:\s+(?'win'\d+))+(?:\s\|)(?:\s+(?'you'\d+))+$",
+    RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+
+var wins = (
+    from line in File.ReadAllLines(@"..\..\..\..\input.txt")
+    from m in regex.Matches(line)
+    let win = m.Groups["win"].Captures.Select(c => int.Parse(c.Value))
+    let you = m.Groups["you"].Captures.Select(c => int.Parse(c.Value))
+    select win.Intersect(you).Count()
+).ToArray();
+
+var cards = new int[wins.Length];
+Array.Fill(cards, 1);
+for (var i = 0; i < cards.Length; i++)
+for (var j = i + 1; j <= i + wins[i] && j < cards.Length; j++)
+    cards[j] += cards[i];
+
+var sum = cards.Sum();
+Debug.Assert(8477787 == sum);
+Console.WriteLine(sum);
