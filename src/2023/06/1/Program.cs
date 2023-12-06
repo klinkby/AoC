b44e4bae66fe4bd134dd077f0e5fc2e4484fd0a6
Mads Klinkby <m@kli.dk>
2023-12-06 15:42:05 +0100

    Nicer parser

--- src/2023/06/1/Program.cs
+++ src/2023/06/1/Program.cs
@@ -4,25 +4,20 @@
 // Multiply the margins to find the power.
 // tRace = duration - tHold = distance / tHold 
 
-using Klinkby.Toolkitt;
-
 var regex = new Regex(@"(\d+)", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
-(
-    from line in File.ReadAllLines(@"../input.txt")
-    select regex
-        .Matches(line)
-        .SelectMany(m => m.Groups[0].Captures.Select(x => int.Parse(x.Value)))
-        .ToArray()
-).TryToValueTuple(out (int[] time, int[] distance) data);
+using var reader = new StreamReader(@"../input.txt");
+var (time, distance) = (ParseLine(reader), ParseLine(reader));
 
-var margin = from i in Enumerable.Range(0, data.time.Length)
-    let x = Math.Sqrt(Math.Pow(data.time[i], 2) - 4 * data.distance[i])
+var margin = from i in Enumerable.Range(0, time.Count)
+    let x = Math.Sqrt(Math.Pow(time[i], 2) - 4 * distance[i])
     let tHold = (
-        (int)Math.Ceiling((data.time[i] - x) / 2),
-        (int)Math.Floor((data.time[i] + x) / 2)
+        (int)Math.Ceiling((time[i] - x) / 2),
+        (int)Math.Floor((time[i] + x) / 2)
     )
     select tHold.Item2 - tHold.Item1 + 1;
 
 var power = margin.Aggregate(1, (a, b) => a * b);
 Debug.Assert(114400 == power);
 Console.WriteLine(power);
+
+IList<int> ParseLine(TextReader rd) => regex.Matches(rd.ReadLine()!).Select(m => int.Parse(m.Value)).ToArray();
--- src/2023/06/2/Program.cs
+++ src/2023/06/2/Program.cs
@@ -2,23 +2,18 @@
 // Solve the hyperbola equation for tHold, quantize and subtract
 // to find the margin to win the race.
 
-using Klinkby.Toolkitt;
-
 var regex = new Regex(@"(\d+)", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
-(
-    from line in File.ReadAllLines(@"../input.txt")
-    select regex
-        .Matches(line)
-        .SelectMany(m => m.Groups[0].Captures.Select(x => long.Parse(x.Value)))
-        .Aggregate((a, b) => long.Parse($"{a}{b}"))
-).TryToValueTuple(out (long time, long distance) data);
+using var reader = new StreamReader(@"../input.txt");
+var (time, distance) = (ParseLine(reader), ParseLine(reader));
 
-var x = Math.Sqrt(Math.Pow(data.time, 2) - 4 * data.distance);
+var x = Math.Sqrt(Math.Pow(time, 2) - 4 * distance);
 var tHold = (
-    (int)Math.Ceiling((data.time - x) / 2),
-    (int)Math.Floor((data.time + x) / 2)
+    (int)Math.Ceiling((time - x) / 2),
+    (int)Math.Floor((time + x) / 2)
 );
 var margin = tHold.Item2 - tHold.Item1 + 1;
 
 Debug.Assert(21039729 == margin);
 Console.WriteLine(margin);
+
+long ParseLine(TextReader rd) => long.Parse(string.Join("", regex.Matches(rd.ReadLine()!).Select(m => m.Value)));
