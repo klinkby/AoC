9894e1b356a836dcd28b39e918bb4fddefaec40e
Mads Klinkby <m@kli.dk>
2023-12-15 15:49:22 +0100

    Day 15 puzzle 1

---
+++ src/2023/15/1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Read the input text file and parse sequences.
+// Summarize the aggregated ASCII values * 17 % 256. 
+
+var reg = new Regex("(?'seq'[^,]+)(?:,|$)", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+var input = File.ReadLines("../input.txt").First();
+var sum = (
+    from m in reg.Matches(input)
+    select m.Groups["seq"].Value.Aggregate(0L, (a, b) => (a + b) * 17 % 256)
+).Sum();
+
+Debug.Assert(510792 == sum);
+Console.WriteLine(sum);
---
+++ src/2023/15/README.md
@@ -0,0 +1,4 @@
+Day 15: Lens Library
+---
+
+https://adventofcode.com/2023/day/15
