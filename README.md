33f0acd67e9e27c9aec89e5b269c666706ba692c
Mads Klinkby <m@kli.dk>
2024-11-19 20:47:50 +0100

    feat: add 2024 boilerplate

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Klinkby.AoC
+# Klinkby' AoC
 
 Solutions in C# for [Advent of code puzzles](https://adventofcode.com/).
 
-- [2023](src/2023)
+- [2023](src/2023) are a bunch of individual projects, one for each day.
+- [2024](src/2024) use a single TUnit testing project for minimal boilerplate, and static bound inputs instead of files.
---
+++ src/2024/EmbeddedResourceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Klinkby.AoC2024;
+
+internal static class EmbeddedResourceExtensions
+{
+    public static IReadOnlyList<string> ReadAllLines(this EmbeddedResource embeddedResource)
+    {
+        using Stream stream = embeddedResource.GetStream();
+        using StreamReader sr = new(stream, Encoding.UTF8, leaveOpen: true);
+        List<string> lines = [];
+        while (!sr.EndOfStream)
+        {
+            string? line = sr.ReadLine();
+            if (line is null)
+            {
+                continue;
+            }
+
+            lines.Add(line);
+        }
+
+        return lines.AsReadOnly();
+    }
+}
