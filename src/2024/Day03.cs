3f489c9c3b8146158ec9c5ad9bed457131c30c78
Mads Klinkby <m@kli.dk>
2024-12-03 21:21:53 +0100

    Day 3: Mull It Over

---
+++ src/2024/Day03.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Klinkby.AoC2024;
+
+public partial class Day03
+{
+    private readonly string _input = EmbeddedResource.input_03_txt.ReadToEnd();
+
+    [Test]
+    public async Task Puzzle1()
+    {
+        int sum = ParseEnabledMuls(_input, forceEnable: true);
+        await Assert.That(sum).IsEqualTo(171183089);
+    }
+
+    [Test]
+    public async Task Puzzle2()
+    {
+        int sum = ParseEnabledMuls(_input);
+        await Assert.That(sum).IsEqualTo(63866497);
+    }
+
+    private static int ParseEnabledMuls(ReadOnlySpan<char> text, bool forceEnable = false)
+    {
+        int sum = 0;
+        bool enabled = true;
+        foreach (ValueMatch token in EnablerMulFinder().EnumerateMatches(text))
+        {
+            ReadOnlySpan<char> value = text.Slice(token.Index, token.Length);
+            switch (value)
+            {
+                case "do()":
+                    enabled = true;
+                    break;
+                case "don't()":
+                    enabled = false;
+                    break;
+                default:
+                    if (enabled || forceEnable)
+                    {
+                        sum += ParseMul(value);
+                    }
+                    break;
+            }
+        }
+
+        return sum;
+    }
+
+    private static int ParseMul(ReadOnlySpan<char> value)
+    {
+        int separatorIndex = value.IndexOf(',');
+        return 
+            int.Parse(value[4..separatorIndex], CultureInfo.InvariantCulture) *
+            int.Parse(value[(separatorIndex + 1)..^1], CultureInfo.InvariantCulture);
+    }
+    
+    [GeneratedRegex(@"do\(\)|don't\(\)|mul\(\d\d?\d?,\d\d?\d?\)",
+        RegexOptions.CultureInvariant | RegexOptions.NonBacktracking)]
+    private static partial Regex EnablerMulFinder();
+}
--- src/2024/Extensions/EmbeddedResourceExtensions.cs
+++ src/2024/Extensions/EmbeddedResourceExtensions.cs
@@ -6,27 +6,22 @@ namespace Klinkby.AoC2024.Extensions;
 
 internal static partial class EmbeddedResourceExtensions
 {
+    public static string ReadToEnd(this EmbeddedResource embeddedResource)
+    {
+        using Stream stream = embeddedResource.GetStream();
+        using StreamReader sr = new(stream, Encoding.UTF8, leaveOpen: true);
+        return sr.ReadToEnd();
+    }
+    
     public static IReadOnlyList<IReadOnlyList<int>> ReadAllIntegers(this EmbeddedResource embeddedResource)
     {
         List<IReadOnlyList<int>> rows = new(1000);
-        rows.AddRange(EnumerateLines(embeddedResource).Select(ParseCells));
+        rows.AddRange(ReadLines(embeddedResource).Select(ParseCells));
 
         return rows;
     }
 
-    private static List<int> ParseCells(string line)
-    {
-        List<int> cells = new(10);
-        foreach (Range range in SpaceSplitter().EnumerateSplits(line))
-        {
-            int integer = int.Parse(line[range], CultureInfo.CurrentCulture);
-            cells.Add(integer);
-        }
-
-        return cells;
-    }
-
-    private static IEnumerable<string> EnumerateLines(this EmbeddedResource embeddedResource)
+    private static IEnumerable<string> ReadLines(this EmbeddedResource embeddedResource)
     {
         using Stream stream = embeddedResource.GetStream();
         using StreamReader sr = new(stream, Encoding.UTF8, leaveOpen: true);
@@ -42,6 +37,18 @@ internal static partial class EmbeddedResourceExtensions
         }
     }
 
-    [GeneratedRegex(@"\s+", RegexOptions.CultureInvariant)]
+    private static List<int> ParseCells(string line)
+    {
+        List<int> cells = new(10);
+        foreach (Range range in SpaceSplitter().EnumerateSplits(line))
+        {
+            int integer = int.Parse(line[range], CultureInfo.CurrentCulture);
+            cells.Add(integer);
+        }
+
+        return cells;
+    }
+    
+    [GeneratedRegex(@"\s+", RegexOptions.CultureInvariant | RegexOptions.NonBacktracking)]
     private static partial Regex SpaceSplitter();
 }
