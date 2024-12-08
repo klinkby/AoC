001d7bad6c287241c838c0fbc204f88dbd44ce3b
Mads Klinkby <m@kli.dk>
2024-12-08 09:51:35 +0100

    Day 8: Resonant Collinearity

---
+++ src/2024/Day08.cs
@@ -0,0 +1,45 @@
+﻿namespace Klinkby.AoC2024;
+
+public class Day08
+{
+    private static readonly char[][] _map = EmbeddedResource.input_08_txt.ReadAllLines()
+        .Select(static x => x.ToCharArray())
+        .ToArray();
+    private static readonly Size _mapSize = new(_map[0].Length, _map.Length);
+
+    [Test]
+    public async Task Puzzle1()
+    {
+        IEnumerable<Point[]> antinodes =
+            from char ch in GetAntennas()
+            let antennas = GetAntennaPositions(ch).ToArray()
+            from posIndex in antennas.Select(static (pos1, index) => (pos1, index))
+            let pos1 = posIndex.pos1
+            from pos2 in antennas.Skip(posIndex.index + 1)
+            let delta = new Size(pos2.X - pos1.X, pos2.Y - pos1.Y)
+            select new[] { pos1 - delta, pos2 + delta };
+        int count = antinodes
+            .SelectMany(static x => x)
+            .Distinct()
+            .Count(InBounds);
+
+        await Assert.That(count).IsEqualTo(381);
+    }
+
+    private static IEnumerable<char> GetAntennas()
+    {
+        return _map.SelectMany(static x => x).Where(static ch => ch != '.').Distinct().Order();
+    }
+
+    private static IEnumerable<Point> GetAntennaPositions(char antenna)
+    {
+        return _map.SelectMany((row, y) =>
+            row.Select((_, x) => new Point(x, y))
+                .Where(p => _map[p.Y][p.X] == antenna));
+    }
+
+    private static bool InBounds(Point p)
+    {
+        return p is { X: >= 0, Y: >= 0 } && p.X < _mapSize.Width && p.Y < _mapSize.Height;
+    }
+}
