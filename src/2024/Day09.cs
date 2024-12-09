8a21f63e0e9749dd7c6e16588fa1e5d402daf8ad
Mads Klinkby <m@kli.dk>
2024-12-09 22:02:22 +0100

    Day 9: Disk Fragmenter

---
+++ src/2024/Day09.cs
@@ -0,0 +1,63 @@
+﻿namespace Klinkby.AoC2024;
+
+public class Day09
+{
+    private const char ZeroAscii = '0';
+    private const ushort Free = ushort.MaxValue;
+
+    [Test]
+    public async Task Puzzle1()
+    {
+        Span<ushort> blocks = GetBlocks();
+        Defragment(blocks);
+
+        long sum = CalculateChecksum(blocks);
+        await Assert.That(sum).IsEqualTo(6367087064415);
+    }
+
+    private static Span<ushort> GetBlocks()
+    {
+        string DenseDisk = EmbeddedResource.input_09_txt
+            .ReadAllLines()
+            .First();
+        Span<ushort> blocks = DenseDisk
+            .ToCharArray()
+            .Select(static (ch, i) => Enumerable.Repeat((ushort)(i % 2 == 0 ? i / 2 : Free), ch - ZeroAscii))
+            .SelectMany(static x => x)
+            .ToArray();
+        return blocks;
+    }
+
+    private static void Defragment(Span<ushort> blocks)
+    {
+        int freeIndex = 0;
+        for (int fileIndex = blocks.Length - 1; fileIndex >= 0; fileIndex--)
+        {
+            if (blocks[fileIndex] == Free)
+            {
+                continue;
+            }
+
+            freeIndex += blocks[freeIndex..].IndexOf(Free);
+            if (freeIndex < 0 || freeIndex >= fileIndex)
+            {
+                break;
+            }
+
+            blocks[freeIndex] = blocks[fileIndex];
+            blocks[fileIndex] = Free;
+            freeIndex++;
+        }
+    }
+    
+    private static long CalculateChecksum(ReadOnlySpan<ushort> blocks)
+    {
+        long sum = 0;
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            sum += blocks[i] == Free ? 0 : blocks[i] * i;
+        }
+
+        return sum;
+    }
+}
