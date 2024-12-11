72fe7f5afaabc6d9ef0e3b9859142757594ebfa2
Mads Klinkby <m@kli.dk>
2024-12-11 08:15:33 +0100

    Day 11: Plutonian Pebbles

---
+++ src/2024/Day11.cs
@@ -0,0 +1,35 @@
+﻿namespace Klinkby.AoC2024;
+
+public class Day11
+{
+    [Test]
+    public async Task Puzzle1()
+    {
+        string stones = EmbeddedResource.input_day11_txt.ReadAllLines().First();
+        for (int i = 0; i < 25; i++)
+        {
+            stones = stones
+                .Split(" ")
+                .Select(BlinkStone)
+                .Aggregate("", static (a, b) => a + b)
+                .TrimEnd();
+        }
+
+        long count = stones.Split(" ").Length;
+        await Assert.That(count).IsEqualTo(217443);
+    }
+
+    private static string BlinkStone(string stone)
+    {
+        bool zero = stone is ['0'];
+        bool evenDigits = stone.Length % 2 == 0;
+        string result = (zero, evenDigits) switch
+        {
+            (true, _) => "1 ",
+            (_, true) =>
+                $"{long.Parse(stone[.. (stone.Length / 2)], CultureInfo.InvariantCulture).ToString(CultureInfo.InvariantCulture)} {long.Parse(stone[(stone.Length / 2) ..], CultureInfo.InvariantCulture).ToString(CultureInfo.InvariantCulture)} ",
+            _ => $"{(long.Parse(stone, CultureInfo.InvariantCulture) * 2024).ToString(CultureInfo.InvariantCulture)} "
+        };
+        return result;
+    }
+}
