cfb50beeac47665c51dba583ec2c9ecc99c312c8
Mads Klinkby <m@kli.dk>
2024-12-20 20:16:29 +0100

    Day 19: Linen Layout

--- src/2024/Day19.cs
+++ src/2024/Day19.cs
@@ -4,14 +4,12 @@ public partial class Day19
 {
     private readonly static string[] Input = EmbeddedResource.input_day19_txt.ReadAllLines().ToArray();
     private readonly static string[] Towels = GetSortedTowels();
+    private readonly static string[] Patterns = GetPatterns();
 
     [Test]
     public async Task Puzzle1()
     {
-        var patterns = Input
-            .SkipWhile(x => x.Length > 0)
-            .Skip(1);
-        int count = patterns.Count(CombinesFromTowels);
+        int count = Patterns.Count(CombinesFromTowels);
         await Assert.That(count).IsEqualTo(315);
     }
 
@@ -27,22 +25,27 @@ public partial class Day19
         while (--i >= 0)
         {
             string towel = Towels[i];
-            if (towel.Length < pattern.Length &&
-                towel == pattern[..towel.Length]
-                && CombinesFromTowels(pattern[towel.Length..]))
-            {
-                return true;
-            }
+            if (towel.Length > pattern.Length || towel != pattern[..towel.Length]) continue;
+            if (towel[0] != pattern[0]) break;
+            if (CombinesFromTowels(pattern[towel.Length..])) return true;
         }
 
         return false;
     }
 
+    private static string[] GetPatterns()
+    {
+        return Input
+            .SkipWhile(static x => x.Length > 0)
+            .Skip(1)
+            .ToArray();
+    }
+    
     private static string[] GetSortedTowels()
     {
         string[] towels = Input
-            .TakeWhile(x => x.Length != 0)
-            .Aggregate("", (acc, x) => acc + x + ", ")
+            .TakeWhile(static x => x.Length != 0)
+            .Aggregate("", static (acc, x) => acc + x + ", ")
             .Split(", ", StringSplitOptions.RemoveEmptyEntries);
         Array.Sort(towels);
         return towels;
